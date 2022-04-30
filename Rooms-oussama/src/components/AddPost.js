a707aa091b95f96e76173bafefe7f20bff7f9baf
Ayoubon <aarrasseayoub@gmail.com>
2022-04-30 18:45:11 +0000

    postii m3a rask

--- Rooms-oussama/src/components/AddPost.js
+++ Rooms-oussama/src/components/AddPost.js
@@ -1,23 +1,40 @@
-import React from "react";
+import React, {useRef, useContext} from "react";
 import profileimage from "../images/profile.png"
 import {BsCardImage} from "react-icons/bs"
+import axios from "axios"
+import { AuthContext } from "../Context/authContext";
+
 
 export default function AddPost() {
+    const desc = useRef()
+    const {user} = useContext(AuthContext)
+
+    const handlePost = async (e) => {
+        // e.preventDefault();
+        const user1 = {desc:desc.current.value, userId:user._id}
+            try{
+                await axios.post("http://localhost:5000/api/posts",user1);
+
+            }catch(err){
+                 console.log(err)        
+            }
+    }
     return(
         <div className="post-add">
             <div className="add-post-title">
                 <h3>Add a Post</h3>
             </div>
+            <form>
             <div className="add-post">
                 <img src={profileimage} className="profileimage" />
-                <div className="add-post-text">
-                    <textarea className="add-post-textarea" placeholder="What's on your mind?" />
-                </div>
+                {/* <div className="add-post-text"> */}
+                <input className="add-post-textarea" type="textarea" placeholder="What's on your mind?" ref={desc}/>
+                {/* </div> */}
                 <button className="add-image"><BsCardImage /></button>
             </div>
-            <div className="add-submit">
-                <button>Post</button>
-            </div>
+            <input className="add-submit" value="post" type="submit" onClick={handlePost}/>
+
+            </form>
         </div>
     )
 }

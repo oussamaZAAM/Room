9995b6df1af70aab1cf3752a94c3ea7c62bc6182
Ayoubon <aarrasseayoub@gmail.com>
2022-05-03 11:09:18 +0000

    l

--- Rooms-oussama/src/App.js
+++ Rooms-oussama/src/App.js
@@ -2,6 +2,7 @@ import React, { useContext, useState } from "react"
 import Login from "./components/Login"
 import Register from "./components/Register"
 import Feed from "./components/Feed"
+import SearchResult from "./components/SearchResult"
 import Profile from "./components/Profile"
 import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
 import { AuthContext } from "./Context/authContext";
@@ -27,6 +28,7 @@ export default function App() {
           <Route path="/login" element={user ? <Feed /> : <Login />} />
           <Route path="/register" element={user ? <Feed /> : <Register />} />
           <Route path="/" element={user ? <Feed /> : <Login />} />
+          <Route path="/allusers" element={user ? <SearchResult /> : <Login />} />
           <Route path="/profile" element={user ? <Profile /> : <Login />} />
           <Route path="/:id" element={user 
               ? <HandleProfile />

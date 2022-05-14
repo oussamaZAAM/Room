69aadaf1239766868a5dc07624cefa030426ed08
oussamaZAAM <zaam.oussama@gmail.com>
2022-05-14 16:15:13 +0100

    name

--- Server/routes/posts.js
+++ Server/routes/posts.js
@@ -6,9 +6,9 @@ const router = express.Router();
 //les urls qu'on peut acceder concernant les posts.
 router.get('/:id', getPost);
 router.post('/', createPost);
-router.get('/profile/:username', allPosts)
+router.get('/profile/:username', profilePosts)
 router.get("/timeline/:userId", feedPosts)
-router.get("/profile1/:userId", profilePosts)
+// router.get("/profile1/:userId", profilePosts)
 router.put("/:id", updatePost)
 router.delete("/:id", deletePost)
 export default router;
